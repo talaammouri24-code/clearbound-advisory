32d6eea0f5178f5c95f9920186545bf64ec803af
talaammouri24-code <tala.ammouri24@gmail.com>
2026-01-06 01:59:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { metaImagesPlugin } from "./vite-plugin-meta-images";
+import { metaImagesPlugin } from "./vite-plugin-meta-images.ts";
 
 export default defineConfig({
   plugins: [
